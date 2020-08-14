3abcdb8490e9a4350dd4e355b2a6169a23ce2c25
Rishab Kumar Bakshi <rishabkbakshi@gmail.com>
2020-08-13 21:54:04 -0500

    module 5 assignment I

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';  
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,18 +11,31 @@ import { DefaultColorOnEventDirective } from './directives/color.directive';
 import { NewemployeeComponent } from './newemployee/newemployee.component';
 
 import { ApiService } from './services/api.service';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
+
+const ROUTES: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'contact', component: ContactComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     // EmployeeListComponent,
     DefaultColorOnEventDirective,
-    NewemployeeComponent
+    // NewemployeeComponent,
+    HomeComponent,
+    ProductComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
